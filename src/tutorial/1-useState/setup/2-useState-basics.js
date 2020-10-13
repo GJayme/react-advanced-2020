e306381d01209c125724567a9eb2d0d386ab38b6
Gabriel Jayme <gabrielj_89@hotmail.com>
2020-10-13 13:23:47 -0300

    feat: useState

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,30 @@
 import React, { useState } from 'react';
+// useState - function
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  // console.log(useState('hello word'));
+  // const value = useState(1)[0];
+  // const handler = useState(1)[1];
+  // console.log(value, handler);
+
+  const [text, setText] = useState('random title');
+
+  const handlerClick = () => {
+    if (text === 'random title') {
+      setText('hello world');
+    } else {
+      setText('random title');
+    }
+  };
+
+  return (
+    <React.Fragment>
+      <h2>{text}</h2>
+      <button className="btn" onClick={handlerClick}>
+        change title
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default UseStateBasics;
